1a7a50b38cba06cb946c709c70e9d27c2c25cba2
NAGOTHANEKAR Vaishnavi <VNR5@dsone.3ds.com>
2022-07-27 16:32:48 +0530

    Add project files.

---
+++ TestCustomer/Customer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace TestCustomer
+{
+    public class Customer 
+    {
+        protected int customer_id;
+        protected string customer_name;
+        protected string customer_address;
+        protected string phone;
+        protected string city;
+        protected string state;
+        protected int pincode;
+        protected string emailid; 
+        
+        public int Cust_id
+        {
+            set
+            {
+                customer_id = value;
+            }
+            get
+            {
+                return customer_id;
+            }
+        }
+
+        public string Cust_name
+        { 
+            set
+            {
+                customer_name = value;
+            }
+            get
+            {
+                return customer_name;
+            }
+        }
+
+        public string Cust_address
+        {
+            set
+            {
+                customer_address = value;
+            }
+            get
+            {
+                return customer_address;
+            }
+        }
+
+        public string Cust_phone
+        {
+            set
+            {
+                phone = value;
+            }
+            get
+            {
+                return phone;
+            }
+        }
+
+        public string Cust_city
+        {
+            set
+            {
+                city = value;
+            }
+            get
+            {
+                return city;
+            }
+        }
+
+        public string Cust_state
+        {
+            set
+            {
+                state = value;
+            }
+            get
+            {
+                return state;
+            }
+        }
+        public int Cust_pincode
+        {
+            set
+            {
+                pincode = value;
+            }
+            get
+            {
+                return pincode;
+            }
+        }
+
+        public string Cust_email
+        {
+            set
+            {
+                emailid = value;
+            }
+            get
+            {
+                return emailid;
+            }
+        }
+
+
+
+    }
+
+}
---
+++ TestCustomer/CustomerArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCustomer
+{
+    class CustomerArray : Customer
+    {
+        Customer[] cust = new Customer[10];
+
+        public int AddCustomer()
+        {
+
+            return customer_id;
+        }
+    }
+}
